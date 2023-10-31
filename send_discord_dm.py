27d8f58d0eee5835181d104dee77159311b35ae0
fal7w <f.alfalahi@fintechsys.net>
2023-10-31 20:27:23 +0300

    script take discord tocken and response person

---
+++ send_discord_dm.py
@@ -0,0 +1,20 @@
+import discord
+import sys
+import json
+
+async def send_dm(token, response_persons):
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        for person in response_persons:
+            user = await client.fetch_user(person['id'])
+            await user.send(person['messge'])
+        await client.close()
+
+    await client.start(token)
+
+
+tocken =sys.argv[1]
+response_persons = json.load(sys.argv[2])
+discord.run(send_dm(token, response_persons))   
