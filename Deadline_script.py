614c72ae801e9c57feff73a801884a97fe6a09b3
fal7w <147312030+fal7w@users.noreply.github.com>
2023-11-07 19:24:25 +0300

    Update Deadline_script.py

--- Deadline_script.py
+++ Deadline_script.py
@@ -15,7 +15,7 @@ response = requests.get(url, headers=headers)
 projects = response.json()
 
 # Iterate over projects and check deadline
-deadline_str = 'Nov 7, 2023'
+deadline_str = ('Nov 7, 2023')
 deadline = datetime.datetime.strptime(deadline_str, '%b %d, %Y').date()
 today = datetime.date.today()
 if today == deadline:
