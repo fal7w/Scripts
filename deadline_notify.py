4ba2b9e2b07dec73ec2f986a12102485aba497d0
fal7w <147312030+fal7w@users.noreply.github.com>
2023-11-08 13:42:58 +0300

    Update deadline_notify.py

--- deadline_notify.py
+++ deadline_notify.py
@@ -1,37 +1,70 @@
-import os
-import sys
-import datetime
-# from dateutil.parser import parse
-from github import Github
-from github.GithubException import BadCredentialsException
-from github.GithubException import UnknownObjectException
-from github.GithubException import RateLimitExceededException
+import requests
+from datetime import datetime
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
 
+# GitHub API endpoint
+BASE_URL = "https://api.github.com"
 
-token = os.getenv("C6SwzYTFQzx5yleCtVT0snlmdLMjuZBtSd55j2VFinE")
-repository_owner = os.getenv("fal7w")
-repository_name = os.getenv("Scripts")
+# GitHub project information
+OWNER = "fal7w"
+REPO = "Scripts"
 
-def get_project_data():
-    try:
-        g = Github(token)
-        repo = g.get_repo(f"{repository_owner}/{repository_name}")
-        projects = repo.get_projects(state="open")
-        return projects
-    except (BadCredentialsException, UnknownObjectException, RateLimitExceededException) as e:
-        print(f"Error accessing repository: {str(e)}")
-        sys.exit(1)
+# Email configuration
+# SMTP_HOST = "mail.fintechsys.net"
+# SMTP_PORT = 465
+# EMAIL_FROM = "github-actions@gmail.com"
+# EMAIL_TO = "f.alfalahi@fintechsys.net"
+# EMAIL_SUBJECT = "GitHub Project Status"
 
-def notify_deadlines(projects):
-    today = datetime.date.today()
-    for project in projects:
-        if project.updated_at.date() == today:
-            print(f"Project '{project.name}' has a deadline today!")
-        elif project.due_date and project.due_date.date() == today:
-            print(f"Project '{project.name}' has a deadline today!")
+# GitHub personal access token
+TOKEN = "C6SwzYTFQzx5yleCtVT0snlmdLMjuZBtSd55j2VFinE"
 
+def get_project_status():
+    url = f"{BASE_URL}/repos/{OWNER}/{REPO}/projects/2"
+    headers = {"Authorization": f"Bearer {TOKEN}"}
 
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        projects = response.json()
+        if projects:
+            project = projects[0]  # Assuming the first project is the one of interest
+            project_name = project["name"]
+            project_url = project["html_url"]
+            project_state = project["state"]
+            project_created_at = project["created_at"]
+            project_updated_at = project["updated_at"]
+
+            status = f"Project: {project_name}\n"
+            status += f"URL: {project_url}\n"
+            status += f"State: {project_state}\n"
+            status += f"Created at: {project_created_at}\n"
+            status += f"Updated at: {project_updated_at}\n"
+
+            return status
+
+    return None
+
+# def send_email_notification(content):
+    message = MIMEText(content)
+    message["Subject"] = EMAIL_SUBJECT
+    message["From"] = EMAIL_FROM
+    message["To"] = EMAIL_TO
+
+    with SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(EMAIL_FROM, "your_email_password")
+        server.send_message(message)
+
+def main():
+    project_status = get_project_status()
+    # if project_status:
+        # send_email_notification(project_status)
+        # print("Notification sent successfully!")
+    # else:
+    #     print("Failed to retrieve project status.")
 
 if __name__ == "__main__":
-    projects = get_project_data()
-    notify_deadlines(projects)
+    main()
+
+
+
